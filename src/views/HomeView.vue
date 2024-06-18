27d5b71a2229e325b9aa810e34ae639f3cc30e17
mrizkya3 <udamuhammadrizky@gmail.com>
2024-06-18 15:35:42 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+     
+    <HeaderView/>
+    <HeroShop/>
+    <WomanShop/>
+    <InstagramShop/>
+    <PartnerShop/>
+    <FooterShop/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderView from '@/components/HeaderView.vue'
+import HeroShop from '@/components/HeroShop.vue'
+import WomanShop from '@/components/WomanShop.vue'
+import InstagramShop from '@/components/InstagramShop.vue'
+import PartnerShop from '@/components/PartnerShop.vue'
+import FooterShop from '@/components/FooterShop.vue'
+
+
+
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderView,HeroShop,WomanShop,InstagramShop,PartnerShop,FooterShop
+  }
+}
+</script>
